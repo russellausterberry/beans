2d9bda16cf55c79fbd92eb631a4bae7a984004e1
Russell <rausterberry@gmail.com>
2020-01-19 21:29:47 +1000

    hooked up bulma-o-steps extension and stubbed 'make available' steps

--- beans-vue/src/features/MakeAvailable.vue
+++ beans-vue/src/features/MakeAvailable.vue
@@ -1,20 +1,59 @@
 <template>
   <section class="section has-text-centered">
-    <span class="tag is-medium is-light">Make available</span>
-    <div class="tabs is-small is-centered">
-      <ul>
-        <li class="is-active"><a href="">select supplier</a></li>
-        <li><a href="">map fields</a></li>
-        <li><a href="">skip items</a></li>
-        <li><a href="">split items</a></li>
-        <li><a href="">add categories</a></li>
-        <li><a href="">cull selection</a></li>
-      </ul>
-    </div>
-    <ul class="steps">
-      <li class="steps-segment"><a href="#" class="steps-marker is-active">1</a></li>
-      <li class="setps-segment"><a href="#" class="steps-marker">2</a></li>
+  <a class="button subtitle is-4">Make Available</a> 
+
+    <ul class="steps has-content-centered">
+      <li class="steps-segment is-active">
+        <span class="steps-marker"></span>
+        <div class="steps-content">
+          <p class="subtitle is-5">Supplier</p>
+          <p>Choose supplier to set availability for</p>
+        </div>
+      </li>
+      <li class="steps-segment">
+        <span class="steps-marker"></span>
+        <div class="steps-content">
+          <p class="subtitle is-5">Method</p>
+          <p>Choose how to set availability</p>
+        </div>
+      </li>
+      <li class="steps-segment">
+        <span class="steps-marker"></span>
+        <div class="steps-content">
+          <p class="subtitle is-">Map</p>
+          <p>If needed match supplier fields to relevant import fields</p>
+        </div>
+      </li>
+      <li class="steps-segment">
+        <span class="steps-marker"></span>
+        <div class="steps-content">
+          <p class="subtitle is-5">Skip</p>
+          <p>If needed check which items to skip by default</p>
+        </div>
+      </li>
+      <li class="steps-segment">
+        <span class="steps-marker"></span>
+        <div class="steps-content">
+          <p class="subtitle is-5">Split</p>
+          <p>Assign each word of item to relevant field</p>
+        </div>
+      </li>
+      <li class="steps-segment">
+        <span class="steps-marker"></span>
+        <div class="steps-content">
+          <p class="subtitle is-5">Sift</p>
+          <p>Assign each item a category and optionally a packing category</p>
+        </div>
+      </li>
+      <li class="steps-segment">
+        <span class="steps-marker"></span>
+        <div class="steps-content">
+          <p class="subtitle is-5">Cull</p>
+          <p>Uncheck any items you do NOT wish to make available</p>
+        </div>
+      </li>
     </ul>
+
     <SelectSupplier></SelectSupplier>
     <MapFields></MapFields>
     <SkipItems></SkipItems>
