10b132a656df608803cbbc76baab65c837b81af0
Russell <rausterberry@gmail.com>
2020-01-30 09:54:08 +1000

    refactored steps component to generate dynamically

--- beans-vue/src/features/make-available/ma-steps.vue
+++ beans-vue/src/features/make-available/ma-steps.vue
@@ -5,68 +5,33 @@
         <button class="subtitle" @click="stepsVisible=!stepsVisible">^</button>
       </div>
       <hr>
+
       <div class="columns">
-        <a class="column has-text-centered has-text-grey" @click="previous(currentStep)">Previous</a>
-        <a class="column subtitle" >Make Available</a>
-        <a class="column has-text-centered has-text-grey" @click="next(currentStep)">Next</a>
+        <div class="column">
+          <button class="button has-text-grey" @click="previous(currentStep)">Previous</button>
+        </div>
+        <div class="column">Make Available</div>
+        <div class="column">
+          <button class="button has-text-grey" @click="next(currentStep)">Next</button>
+        </div>
       </div>
-
-      <!-- without the following line the 'Previous' and 'Next' buttons don't work (!) -->
-      <p v-show="false">{{ currentStep }}</p>
-
 
       <!-- 'make available' steps progress bar -->
       <ul class="steps has-content-centered">
-        <li class="steps-segment" v-bind:class="{'is-active':stepActive[0]}">
-          <span class="steps-marker">1</span>
+        <li 
+          v-bind:key="step" 
+          v-for="step in steps"
+          class="steps-segment"
+          v-bind:class="{'is-active':step.stepData.stepActive}"
+          >
+          <span class="steps-marker">{{ step.stepData.stepMarker }}</span>
           <div class="steps-content">
-            <p class="subtitle is-5" v-bind:class="{'has-text-grey-light':!stepActive[0]}">Supplier</p>
-            <p v-bind:class="{'is-size-7 has-text-grey-light':!stepActive[0]}">Choose supplier to set availability for</p>
-          </div>
-        </li>
-        <li class="steps-segment" v-bind:class="{'is-active':stepActive[1]}">
-          <span class="steps-marker">2</span>
-          <div class="steps-content">
-            <p class="subtitle is-5" v-bind:class="{'has-text-grey-light':!stepActive[1]}">Method</p>
-            <p v-bind:class="{'is-size-7 has-text-grey-light':!stepActive[1]}">Choose how to set availability</p>
-          </div>
-        </li>
-        <li class="steps-segment" v-bind:class="{'is-active':stepActive[2]}">
-          <span class="steps-marker">3</span>
-          <div class="steps-content">
-            <p class="subtitle is-5" v-bind:class="{'has-text-grey-light':!stepActive[2]}">Map</p>
-            <p v-bind:class="{'is-size-7 has-text-grey-light':!stepActive[2]}">If needed match supplier fields to import fields</p>
-          </div>
-        </li>
-        <li class="steps-segment" v-bind:class="{'is-active':stepActive[3]}">
-          <span class="steps-marker">4</span>
-          <div class="steps-content">
-            <p class="subtitle is-5" v-bind:class="{'has-text-grey-light':!stepActive[3]}">Skip</p>
-            <p v-bind:class="{'is-size-7 has-text-grey-light':!stepActive[3]}">If needed check which items to skip by default</p>
-          </div>
-        </li>
-        <li class="steps-segment" v-bind:class="{'is-active':stepActive[4]}">
-          <span class="steps-marker">5</span>
-          <div class="steps-content">
-            <p class="subtitle is-5" v-bind:class="{'has-text-grey-light':!stepActive[4]}">Split</p>
-            <p v-bind:class="{'is-size-7 has-text-grey-light':!stepActive[4]}">Assign each word of item to relevant field</p>
-          </div>
-        </li>
-        <li class="steps-segment" v-bind:class="{'is-active':stepActive[5]}">
-          <span class="steps-marker">6</span>
-          <div class="steps-content">
-            <p class="subtitle is-5" v-bind:class="{'has-text-grey-light':!stepActive[5]}">Sift</p>
-            <p v-bind:class="{'is-size-7 has-text-grey-light':!stepActive[5]}">Assign each item a category and optionally a packing category</p>
-          </div>
-        </li>
-        <li class="steps-segment" v-bind:class="{'is-active':stepActive[6]}">
-          <span class="steps-marker">7</span>
-          <div class="steps-content">
-            <p class="subtitle is-5" v-bind:class="{'has-text-grey-light':!stepActive[6]}">Cull</p>
-            <p v-bind:class="{'is-size-7 has-text-grey-light':!stepActive[6]}">Uncheck any items you do NOT wish to make available</p>
-          </div>
+            <p class="subtitle is-5" v-bind:class="{'has-text-grey-light':!step.stepData.stepActive}">{{ step.stepData.stepName }}</p>
+            <p v-bind:class="{'is-size-7 has-text-grey-light':!step.stepData.stepActive}">{{ step.stepData.stepInfo }}</p>
+          </div>          
         </li>
       </ul>
+
     </div>
 
     <div>
@@ -80,46 +45,79 @@
   export default {
     data: function() {
       return {
-        currentStep: 1,   // 1 correlates to step 1, etc
-        stepActive: [     // index 0 correlates to step 1, etc
-          true,
-          false,
-          false,
-          false,
-          false,
-          false,
-          false
-        ],
-        componentList: [  // index 0 correlates to step 1 component ("Supplier"), etc
-          "Supplier",
-          "Method",
-          "Map",
-          "Skip",
-          "Split",
-          "Sift",
-          "Cull"
-        ],
-        stepsVisible: true  //controls visibility of entire steps component
+        currentStep: 1,      // 1 correlates to step 1, etc
+        stepsVisible: true,  // controls visibility of entire steps component
+        steps: [             // data for each of the steps
+          { stepData: {
+            stepMarker: 1,
+            stepName: "Supplier",
+            stepActive: true,
+            stepInfo: "Choose supplier to set availability for"
+            }
+          },
+          { stepData: {
+            stepMarker: 2,
+            stepName: "Method",
+            stepActive: false,
+            stepInfo: "Choose whether to import from supplier or change manually"            
+            }
+          },
+          { stepData: {
+            stepMarker: 3,
+            stepName: "Map",
+            stepActive: false,
+            stepInfo: "If needed, map supplier fields to 'beans' fields"
+            }
+          },
+          { stepData: {
+            stepMarker: 4,
+            stepName: "Skip",
+            stepActive: false,
+            stepInfo: "If needed select which items to skip by default"
+            }
+          },
+          { stepData: {
+            stepMarker: 5,
+            stepName: "Split",
+            stepActive: false,
+            stepInfo: "Drag the words of each item into relevant box"
+            }
+          },
+          { stepData: {
+            stepMarker: 6,
+            stepName: "Sift",
+            stepActive: false,
+            stepInfo: "Add categories"
+            }
+          },
+          { stepData: {
+            stepMarker: 7,
+            stepName: "Cull",
+            stepActive: false,
+            stepInfo: "Do a final comparison and uncheck any items you wish to cull"
+            }
+          }
+        ]
       };
     },
     methods: {
       next(pos) {
-        if (pos < this.stepActive.length) { 
-          pos--;                            // convert to 0-based array index
-          this.stepActive[pos] = false;     // reset current step
-          this.stepActive[pos+1] = true;    // activate next step
-          this.currentStep++;               // increment
-          this.$emit('currentStepChanged', this.componentList[pos+1]);
-        }
+        if (pos < this.steps.length) { 
+          pos--;           
+          this.steps[pos].stepData.stepActive = false;     // reset current step
+          this.steps[pos+1].stepData.stepActive = true;    // activate next step
+          this.currentStep++;                              // increment
+          this.$emit('currentStepChanged', this.steps[pos+1].stepData.stepName); 
+        }         
       },
       previous(pos) {
-        if (pos > 1) {                      
-          pos--;                            // convert to 0-based array index
-          this.stepActive[pos] = false;     // reset current step
-          this.stepActive[pos-1] = true;    // activate previous step
-          this.currentStep--;               // decrement
-          this.$emit('currentStepChanged', this.componentList[pos-1]);
-        }
+        if (pos > 1) { 
+          pos--;           
+          this.steps[pos].stepData.stepActive = false;     // reset current step
+          this.steps[pos-1].stepData.stepActive = true;    // activate next step
+          this.currentStep--;                              // decrement
+          this.$emit('currentStepChanged', this.steps[pos-1].stepData.stepName); 
+        }   
       }
     }    
   }
